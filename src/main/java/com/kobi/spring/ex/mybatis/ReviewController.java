91d6e85b1d08dc9e70b6f0617222e8b242c005f3
LAPTOP-DHOJEMI6\석호빈 <govlqlsdl@naver.com>
2025-02-26 23:44:11 +0900

    [석호빈] - mvc 예제 및 테스트

---
+++ src/main/java/com/kobi/spring/ex/mybatis/ReviewController.java
@@ -0,0 +1,53 @@
+package com.kobi.spring.ex.mybatis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kobi.spring.ex.mybatis.domain.Review;
+import com.kobi.spring.ex.mybatis.service.ReviewService;
+
+@RequestMapping("/mybatis/review")
+@Controller
+public class ReviewController {
+	
+	@Autowired
+	private ReviewService reviewService;
+	
+	// 전달받은 id와 일치하는 리뷰정보를 json으로 response에 담는다 
+	@ResponseBody
+	@RequestMapping("")
+	public Review review(@RequestParam("id") int id) {
+		// String idString = request.getParameter("id");
+		// int id = Integer.parseInt(idString);
+		
+		// 전달한 id와 일치하는 리뷰정보 얻어오기
+		Review review = reviewService.getReview(id);
+		
+		return review;
+	}
+	
+	// 리뷰내용을 저장하는 기능
+	@ResponseBody
+	@RequestMapping("/create")
+	public String createReview() {
+		// 4, 치즈피자, 김인규, 4.5 치즈피자 존맛!
+//		int count = reviewService.addReview(4, "치즈피자", "김인규", 4.5, "치즈피자 존맛!");
+		
+		// 2, 뿌링클, 김인규, 4.0, 역시 뿌링클은 진리입니다!!
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리입니다!!");
+		// review 객체의 멤버변수 값을 리뷰내용으로 저장한다. 
+		int count = reviewService.addReviewByObject(review);
+		
+		return "삽입 결과 " + count;
+	}
+	
+
+}
---
+++ src/main/java/com/kobi/spring/ex/mybatis/service/ReviewService.java
@@ -0,0 +1,44 @@
+package com.kobi.spring.ex.mybatis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kobi.spring.ex.mybatis.domain.Review;
+import com.kobi.spring.ex.mybatis.repository.ReviewRepository;
+
+@Service
+public class ReviewService {
+	
+	@Autowired
+	private ReviewRepository reviewRepository;
+	
+	// 전달받은 id와 일치하는 리뷰정보 얻어오기 
+	public Review getReview(int id) {
+		// new_review 테이블에서 전달한 id가 일치하는 행 조회 
+		Review review = reviewRepository.selectReview(id);
+		
+		return review;
+	}
+	
+	// 작성할 내용을 전달 받고 리뷰를 저장하는 기능
+	public int addReview(
+			int storeId
+			, String menu
+			, String userName
+			, double point
+			, String review) {
+		
+		int count = reviewRepository.insertReview(storeId, menu, userName, point, review);
+		
+		return count;
+	}
+	
+	public int addReviewByObject(Review review) {
+		// Review 객체의 멤버변수 값을 new_review 테이블에 저장 
+		int count = reviewRepository.insertReviewByObject(review);
+		
+		return count;
+	}
+	
+
+}
